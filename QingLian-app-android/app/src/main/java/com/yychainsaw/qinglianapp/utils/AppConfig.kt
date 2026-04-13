e697002f892eb93d774d373c72d6d4461a447759
YYchainsAw <2560634677@qq.com>
2026-04-13 17:50:49 +0800

    添加VUE网页前端模块

--- QingLian-app-android/app/src/main/java/com/yychainsaw/qinglianapp/utils/AppConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.yychainsaw.qinglianapp.utils
-
-object AppConfig {
-    private const val DOMAIN = "yychainsaw.xyz:8080"
-    const val BASE_URL = "https://$DOMAIN/"
-    const val WS_URL = "wss://$DOMAIN/ws/websocket"
-}
-
---
+++ qinglian-app-android/app/src/main/java/com/yychainsaw/qinglianapp/utils/AppConfig.kt
@@ -0,0 +1,8 @@
+package com.yychainsaw.qinglianapp.utils
+
+object AppConfig {
+    private const val DOMAIN = "123.207.5.186:8080"
+    const val BASE_URL = "http://$DOMAIN/"
+    const val WS_URL = "ws://$DOMAIN/ws/websocket"
+}
+
